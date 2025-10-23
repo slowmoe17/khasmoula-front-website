29b569d2c9d024aa3555f8f19c6a5150e4ee658c
Mohamed-Saad-02 <mohamedsaad23c@gmail.com>
2025-10-23 23:56:26 +0300

    Add StoreCard component and integrate it into the page layout

--- src/app/stores/page.tsx
+++ src/app/stores/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Filters } from "@/components";
+import { Container, Filters, StoreCard } from "@/components";
 import { Suspense } from "react";
 
 function Page() {
@@ -8,6 +8,10 @@ function Page() {
         <Suspense fallback={<div>Loading...</div>}>
           <Filters title="متاجرنا" placeholder="ابحث عن متجر..." />
         </Suspense>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-9.5">
+          <StoreCard />
+        </div>
       </Container>
     </div>
   );
--- src/components/shared/StoreCard.tsx
+++ src/components/shared/StoreCard.tsx
@@ -1,5 +1,39 @@
+import Image from "next/image";
+import { BookmarkIcon } from "../icons";
+
 function StoreCard() {
-  return <div>StoreCard</div>;
+  return (
+    <div className="bg-white pt-5 rounded-[10px] shadow-[0px_0px_3px_0px_#00000040]">
+      <div className="flex items-center justify-between">
+        <button
+          type="button"
+          className="shadow-[0px_0px_2px_0px_#00000040] ms-5 w-12 h-12 rounded-full overflow-hidden flex items-center justify-center"
+        >
+          <BookmarkIcon className="text-primary size-6" />
+        </button>
+
+        <div className="w-[115px] h-9.5 flex items-center justify-center bg-primary-light rounded-tr-[50px] rounded-br-[50px] mt-[5px] text-primary">
+          الاكثر استعمالا
+        </div>
+      </div>
+
+      <div className="flex items-center justify-center flex-col -mt-[15px]">
+        <Image
+          src="/noon.png"
+          alt="store"
+          width={129}
+          height={129}
+          className="max-w-[129px] max-h-[129px] rounded-full"
+        />
+
+        <h3 className="text-xl font-semibold mt-5">نون</h3>
+
+        <p className="text-center text-[22px] font-semibold">
+          احصل علي خصومات تصل الي 40%
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default StoreCard;
